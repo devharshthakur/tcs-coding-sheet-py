b3dff34f0924a562b1a5d1e67e7ff84a3c0589f3
Harsh Thakur <harshprojects2002@outlook.com>
2026-03-22 13:58:54 +0530

    feat: add element in array

---
+++ arrays/add_element_in_array.py
@@ -0,0 +1,27 @@
+# Add element in an array
+
+
+def solve():
+    n = int(input("Enter number of elements: "))
+    arr = []
+
+    for i in range(n):
+        val = int(input(f"Enter element {i}: "))
+        arr.append(val)
+
+    print(f"Initial array: {arr}")
+
+    adder = int(input("Enter element you want to add: "))
+    index = int(input("Enter at which position you want to add: "))
+
+    arr.append(0)
+
+    for i in range(len(arr) - 1, index, -1):
+        arr[i] = arr[i - 1]
+    arr[index] = adder
+
+    print("Updated array: ")
+    print(arr)
+
+
+solve()
