a219ec42301c4165869d8e5d454fd2c3d310cdbc
Harsh Thakur <harshprojects2002@outlook.com>
2026-04-20 01:48:18 +0530

    fix: `ruff` format error in `lint-stage`

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 /** @type {import('lint-staged').Config} */
 export default {
   "*.{ts,tsx,js,jsx}": ["prettier --write"],
-  "*.py": ["uv format --preview-features format", "uv run ty check"],
+  "*.py": ["uv run ruff format", "uv run ty check"],
   "{*.md,*.json,*.yml,*.yaml}": ["prettier --write"],
 };
